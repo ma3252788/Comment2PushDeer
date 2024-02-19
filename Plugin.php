c53bd16d52530571c2799b8751285305812a87c7
马春杰 <tianbing010@126.com>
2024-02-19 10:24:27 +0800

    初始化

---
+++ Plugin.php
@@ -0,0 +1,101 @@
+
+<?php
+/**
+ * PushDeer推送评论通知
+ *
+ * @package Comment2PushDeer
+ * @author 马春杰
+ * @version 1.0.0
+ * @link https://www.machunjie.com
+ */
+class Comment2PushDeer_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+    
+        Typecho_Plugin::factory('Widget_Feedback')->comment = array('Comment2PushDeer_Plugin', 'sc_send');
+        Typecho_Plugin::factory('Widget_Feedback')->trackback = array('Comment2PushDeer_Plugin', 'sc_send');
+        Typecho_Plugin::factory('Widget_XmlRpc')->pingback = array('Comment2PushDeer_Plugin', 'sc_send');
+        
+        return _t('请配置此插件的 PDKEY, 以使您的PushDeer推送生效');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+    
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $key = new Typecho_Widget_Helper_Form_Element_Text('PDKEY', NULL, NULL, _t('PDKEY'), _t('PDKEY 需要在 <a href="http://pushdeer.com/">PushDeer官网</a> 注册<br />'));
+        $form->addInput($key->addRule('required', _t('您必须填写一个正确的 PDKEY')));
+    }
+    
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    /**
+     * 微信推送
+     *
+     * @access public
+     * @param array $comment 评论结构
+     * @param Typecho_Widget $post 被评论的文章
+     * @return void
+     */
+    public static function sc_send($comment, $post)
+    {
+        $options = Typecho_Widget::widget('Widget_Options');
+
+        $PDKEY = $options->plugin('Comment2PushDeer')->PDKEY;
+
+        $text = "有新评论啦";
+        $desp = "**".$comment['author']."** 在你的博客中说到：\n\n > ".$comment['text'];
+
+
+        $postdata = http_build_query(
+            array(
+                'text' => $text,
+                'desp' => $desp,
+                'pushkey' => $PDKEY,
+                )
+            );
+
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+                )
+            );
+        $context  = stream_context_create($opts);
+        
+        $result = file_get_contents('https://api2.pushdeer.com/message/push', false, $context);
+        
+        return  $comment;
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Comment2PushDeer
+
+将`typecho`博客的评论通过`PushDeer`推送
+
+下载后解压，将文件夹名称改为`Comment2PushDeer`，然后移动到`usr/plugins`目录下即可
+
+配置`PDKEY`即可
+
+
+/*
+ * PushDeer推送评论通知
+ * @package Comment2PushDeer
+ * @author 马春杰
+ * @version 1.0.0
+ * @link https://www.machunjie.com
+ */

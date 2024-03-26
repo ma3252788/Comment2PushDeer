8b20917c864f56f7281961a1c05d7e01f008acf8
马春杰 <tianbing010@126.com>
2024-03-26 18:15:38 +0800

    增加自建host

--- Plugin.php
+++ Plugin.php
@@ -5,7 +5,7 @@
  *
  * @package Comment2PushDeer
  * @author 马春杰
- * @version 1.0.2
+ * @version 1.0.3
  * @link https://www.machunjie.com/opensource/1662.html
  * @link_gitee https://gitee.com/public_sharing/Comment2PushDeer
  * @link_github https://github.com/ma3252788/Comment2PushDeer
@@ -49,7 +49,9 @@ class Comment2PushDeer_Plugin implements Typecho_Plugin_Interface
     public static function config(Typecho_Widget_Helper_Form $form)
     {
         $key = new Typecho_Widget_Helper_Form_Element_Text('PDKEY', NULL, NULL, _t('PDKEY'), _t('PDKEY 需要在 <a href="http://pushdeer.com/">PushDeer官网</a> 注册<br />'));
+        $selfhost = new Typecho_Widget_Helper_Form_Element_Text('SELFHOST', NULL, NULL, _t('自建host地址'), _t('可选参数：用于自建PushDeer服务器用，类似：https://api.xxxxx.com:1234'));
         $form->addInput($key->addRule('required', _t('您必须填写一个正确的 PDKEY')));
+        $form->addInput($selfhost);
     }
     
     /**
@@ -77,7 +79,7 @@ class Comment2PushDeer_Plugin implements Typecho_Plugin_Interface
             $options = Typecho_Widget::widget('Widget_Options');
     
             $PDKEY = $options->plugin('Comment2PushDeer')->PDKEY;
-    
+            $selfhost = $options->plugin('Comment2PushDeer')->SELFHOST;
             $text = "有新评论啦";
             $desp = "**".$comment->author."** 在你的博客中说到：\n\n > ".$comment->text;
     
@@ -98,9 +100,9 @@ class Comment2PushDeer_Plugin implements Typecho_Plugin_Interface
                     )
                 );
             $context  = stream_context_create($opts);
-            
-            $result = file_get_contents('https://api2.pushdeer.com/message/push', false, $context);
-            
+            $baseUrl = empty($selfhost) ? 'https://api2.pushdeer.com/message/push' : rtrim($selfhost, '/') . '/message/push';
+            // $result = file_get_contents('https://api2.pushdeer.com/message/push', false, $context);
+            $result = file_get_contents($baseUrl, false, $context);
             return  $comment;
             
         }
